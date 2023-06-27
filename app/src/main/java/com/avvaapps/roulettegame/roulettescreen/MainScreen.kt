ad901ff7aacfedb6a2b44f8dc6c0fda38b175436
Dss <sd;fjkwoiefjsldkfj@adjskjd.org>
2023-06-27 17:05:48 +0200

    Animation and winning number calculation

--- app/src/main/java/com/avvaapps/roulettegame/roulettescreen/MainScreen.kt
+++ app/src/main/java/com/avvaapps/roulettegame/roulettescreen/MainScreen.kt
@@ -1,5 +1,7 @@
 package com.avvaapps.roulettegame.roulettescreen
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -14,7 +16,12 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -22,15 +29,36 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.avvaapps.roulettegame.R
 import com.avvaapps.roulettegame.ui.theme.RedButton
+import com.avvaapps.roulettegame.utils.NumberUtil
+import kotlin.math.roundToInt
 
 @Composable
 fun MainScreen() {
+
+    var rotaionValue by remember {
+        mutableStateOf(0f)
+    }
+
+    var number by remember {
+        mutableStateOf(0)
+    }
+    val angle: Float by animateFloatAsState(
+        targetValue = rotaionValue,
+        animationSpec = tween(
+            durationMillis = 2000
+//            easing = LinearOutSlowInEasing
+        ),
+        finishedListener = {
+            val index = (360 - (it % 360)) / (360f / NumberUtil.list.size)
+            number  = NumberUtil.list[index.roundToInt()]
+        }
+    )
     Column(
         modifier = Modifier.fillMaxSize(),
         verticalArrangement = Arrangement.SpaceBetween
     ) {
         Text(
-            text = "0",
+            text = number.toString(),
             fontWeight = FontWeight.Bold,
             fontSize = 35.sp,
             color = Color.White,
@@ -48,7 +76,9 @@ fun MainScreen() {
             Image(
                 painter = painterResource(id = R.drawable.roulette),
                 contentDescription = "Roulette",
-                modifier = Modifier.fillMaxSize()
+                modifier = Modifier
+                    .fillMaxSize()
+                    .rotate(angle)
             )
             Image(
                 painter = painterResource(id = R.drawable.arrow),
@@ -57,7 +87,9 @@ fun MainScreen() {
             )
         }
         Button(
-            onClick = {},
+            onClick = {
+                rotaionValue = (720..1080).random().toFloat() + angle
+            },
             colors = ButtonDefaults.buttonColors(containerColor = RedButton),
             modifier = Modifier
                 .fillMaxWidth()
